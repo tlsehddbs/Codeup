ed7d8e5c11fc1d337da851469cd27acb765032ef
tlsehddbs <sdy4277@naver.com>
2021-05-01 23:36:19 +0900

    Func

---
+++ BasicFunction/1535.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int n, d[110];
+
+int f()
+{
+    int i, max = -1;
+    for(i = 0; i < n; i++)
+    {
+        if(d[i] > max)
+            max = d[i];
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(d[i] == max)
+        {
+            return i + 1;
+            break;
+        }
+    }
+}
+
+int main()
+{
+  scanf("%d", &n);
+
+  for(int i=0; i<n; i++)
+    scanf("%d", &d[i]);
+
+  printf("%d", f());
+  return 0;
+}
