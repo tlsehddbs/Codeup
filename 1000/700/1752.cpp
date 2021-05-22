149fe4a1fa40f39488fd02e6a4c42ace7846b284
tlsehddbs <sdy4277@naver.com>
2021-05-23 01:36:29 +0900

    .

---
+++ 1000/700/1752.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char a[20];
+    cin >> a;
+    for(int i = (strlen(a) - 1); i >= 0; i--)
+        cout << a[i];
+    return 0;
+}
---
+++ 2000/000/2001.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float pm, jm;
+    int p[3] = {};
+    int j[2] = {};
+
+    for(int i = 0; i < 3; i++)
+        cin >> p[i];
+    for(int i = 0; i < 2; i++)
+        cin >> j[i];
+
+    pm = ((p[0] >= p[1] ? p[1] : p[0]) >= p[2] ? p[2] : (p[0] >= p[1] ? p[1] : p[0]));
+    jm = (j[0] >= j[1] ? j[1] : j[0]);
+
+    cout << fixed;
+    cout.precision(1);
+    cout << (pm + (pm * 0.1)) + (jm + (jm * 0.1));
+
+    return 0;
+}
