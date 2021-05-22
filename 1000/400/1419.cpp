7026a0afd4298e9e58322bd6b1a3633a5df850eb
tlsehddbs <sdy4277@naver.com>
2021-05-22 22:50:29 +0900

    .

--- 1000/400/1419.cpp
+++ 1000/400/1419.cpp
@@ -4,13 +4,13 @@ int main()
 {
     int count = 0;
     char a[100] = {};
-    std::cin >> a;
+    std::cin.getline(a, 100);
 
     for(int i = 0; a[i] != '\0'; i++)
         if(a[i] == 'l' && a[i+1] == 'o' && a[i+2] == 'v' && a[i+3] == 'e')
             count++;
 
     std::cout << count;
-
+    
     return 0;
 }
