792e1d148a958f6f87ac4a87a49def911f055cae
tlsehddbs <sdy4277@naver.com>
2021-06-05 13:34:55 +0900

    .

---
+++ 1000/200/1291.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    for(int i = a; i > 0; i--)
+    {
+        if(a % i == 0 && b % i == 0 && c % i == 0)
+        {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
